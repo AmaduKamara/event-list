333d1982db2fd7c9f6e2122448714dec43da8087
Amkam <amaduskamara@gmail.com>
2022-01-23 08:08:56 +0000

    Work on responsive for Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ export default function Hero() {
   return (
     <div className="w-full h-48 bg-sky-500 flex justify-center items-center sticky top-0 shadow-md">
       <div className="text-center">
-        <h1 className="text-7xl font-thin text-gray-100">Event Listing</h1>
-        <p className="my-3 text-xl text-gray-200">
+        <h1 className="text-4xl md:text-7xl font-thin text-gray-100">Event Listing</h1>
+        <p className="my-3 md:text-xl text-gray-200">
           Add or view events in your location
         </p>
       </div>
